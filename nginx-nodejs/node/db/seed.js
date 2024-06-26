b29f2746d15a00c7019d56fdec1a127b0cb1a253
genesluna <genesluna@gmail.com>
2024-06-26 12:37:30 -0300

    added conditional and made it async

--- nginx-nodejs/node/db/seed.js
+++ nginx-nodejs/node/db/seed.js
@@ -1,23 +1,30 @@
+import { getPeopleCount } from "../services/getPeople.js";
 import { connection } from "./connection.js";
 
-export const seedDataBase = () => {
+export const seedDataBase = async () => {
+  const peopleCount = await getPeopleCount();
+
+  if (peopleCount > 0) {
+    console.log("Database already seeded.");
+    return;
+  }
+
   console.log("Seeding Database...");
 
   const names = ["João", "Abel", "Juliana", "Niedja", "Carlos", "Bianca"];
 
-  names.forEach((name) => {
-    connection.query(
-      "INSERT INTO people (name) VALUES (?)",
-      [name],
-      (error, results) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(results);
+  await Promise.all(
+    names.map(async (name) => {
+      connection.query(
+        "INSERT INTO people (name) VALUES (?)",
+        [name],
+        (error) => {
+          if (error) {
+            console.log(error);
+          }
         }
-      }
-    );
-  });
-
-  connection.end();
+      );
+    })
+  );
+  console.log("Database seeded.");
 };
